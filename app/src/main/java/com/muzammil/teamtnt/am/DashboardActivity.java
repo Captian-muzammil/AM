21dd918fc487ddeeb760614aee8561524653a8d1
Captian-muzammil <muzammilas7k@gmail.com>
2019-02-10 15:06:35 +0530

    Creating a Custom Layout For DialogFragment with animation

--- app/src/main/java/com/muzammil/teamtnt/am/DashboardActivity.java
+++ app/src/main/java/com/muzammil/teamtnt/am/DashboardActivity.java
@@ -1,8 +1,13 @@
 package com.muzammil.teamtnt.am;
 
+import android.content.res.Resources;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class DashboardActivity extends AppCompatActivity implements NoticeDialogFragment.NoticeDialogListener{
 
@@ -11,15 +16,37 @@ public class DashboardActivity extends AppCompatActivity implements NoticeDialog
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        showNoticeDialog();
-    }
+            TextView textView = findViewById(R.id.hello);
 
-    public void showNoticeDialog() {
-        // Create an instance of the dialog fragment and show it
-        DialogFragment dialog = new NoticeDialogFragment();
-        dialog.show(getSupportFragmentManager(), "NoticeDialogFragment");
-    }
 
+           textView.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                   showDialog();
+               }
+           });
+        }
+
+
+
+    public void showDialog() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        NoticeDialogFragment newFragment = new NoticeDialogFragment();
+
+        if (false) {
+            // The device is using a large layout, so show the fragment as a dialog
+            newFragment.show(fragmentManager, "dialog");
+        } else {
+            // The device is smaller, so show the fragment fullscreen
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            // For a little polish, specify a transition animation
+            transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+            // To make it fullscreen, use the 'content' root view as the container
+            // for the fragment, which is always the root view for the activity
+            transaction.add(android.R.id.content, newFragment)
+                    .addToBackStack(null).commit();
+        }
+    }
     // The dialog fragment receives a reference to this Activity through the
     // Fragment.onAttach() callback, which it uses to call the following methods
     // defined by the NoticeDialogFragment.NoticeDialogListener interface
--- app/src/main/java/com/muzammil/teamtnt/am/NoticeDialogFragment.java
+++ app/src/main/java/com/muzammil/teamtnt/am/NoticeDialogFragment.java
@@ -6,6 +6,10 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 
 public class NoticeDialogFragment extends DialogFragment {
 
@@ -17,25 +21,30 @@ public class NoticeDialogFragment extends DialogFragment {
         public void onDialogNegativeClick(DialogFragment dialog);
     }
 
+    @Override
+    public int getTheme() {
+        return R.style.AppTheme_NoActionBar_FullScreenDialog;
+    }
 
+    /** The system calls this to get the DialogFragment's layout, regardless
+     of whether it's being displayed as a dialog or an embedded fragment. */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout to use as dialog or embedded fragment
+        return inflater.inflate(R.layout.purchase_items, container, false);
+    }
+
+    /** The system calls this only when creating the layout in a dialog. */
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Build the dialog and set up the button click handlers
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage("BOOM")
-                .setPositiveButton("yes", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // Send the positive button event back to the host activity
-                        mListener.onDialogPositiveClick(NoticeDialogFragment.this);
-                    }
-                })
-                .setNegativeButton("NO", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // Send the negative button event back to the host activity
-                        mListener.onDialogNegativeClick(NoticeDialogFragment.this);
-                    }
-                });
-        return builder.create();
+        // The only reason you might override this method when using onCreateView() is
+        // to modify any dialog characteristics. For example, the dialog includes a
+        // title by default, but your custom layout might not need it. So here you can
+        // remove the dialog title, but you must call the superclass to get the Dialog.
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        return dialog;
     }
     // Use this instance of the interface to deliver action events
     NoticeDialogListener mListener;
