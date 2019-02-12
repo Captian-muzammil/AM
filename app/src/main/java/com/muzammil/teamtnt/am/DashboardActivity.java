dd9acf6463bcfb9d5b6ae0aadcadcefd9ac8c87d
Captian-muzammil <muzammilas7k@gmail.com>
2019-02-12 23:30:41 +0530

    reformate calender

--- app/src/main/java/com/muzammil/teamtnt/am/DashboardActivity.java
+++ app/src/main/java/com/muzammil/teamtnt/am/DashboardActivity.java
@@ -1,5 +1,6 @@
 package com.muzammil.teamtnt.am;
 
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -28,23 +29,7 @@ public class DashboardActivity extends AppCompatActivity{
 
 
     public void showDialog() {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        DateRangeDialogFragment newFragment = new DateRangeDialogFragment();
-
-        if (false) {
-            // The device is using a large layout, so show the fragment as a dialog
-            newFragment.show(fragmentManager, "dialog");
-        } else {
-            // The device is smaller, so show the fragment fullscreen
-            FragmentTransaction transaction = fragmentManager.beginTransaction();
-            // For a little polish, specify a transition animation
-            transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-            // To make it fullscreen, use the 'content' root view as the container
-            // for the fragment, which is always the root view for the activity
-            transaction.add(android.R.id.content, newFragment)
-                    .addToBackStack(null).commit();
+        DialogFragment dialog = new DateRangeDialogFragment();
+        dialog.show(getSupportFragmentManager(), "DateRangeDialogFragment");
         }
     }
-
-
-}
