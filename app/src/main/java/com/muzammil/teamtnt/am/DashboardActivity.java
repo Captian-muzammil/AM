5a950e13ea689685b9f428d4d9f94b514a802a86
Captian-muzammil <muzammilas7k@gmail.com>
2019-02-13 01:26:18 +0530

    no internet connection recyclerview menu

--- app/src/main/java/com/muzammil/teamtnt/am/DashboardActivity.java
+++ app/src/main/java/com/muzammil/teamtnt/am/DashboardActivity.java
@@ -1,30 +1,74 @@
 package com.muzammil.teamtnt.am;
 
+import android.content.Context;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
 
 public class DashboardActivity extends AppCompatActivity{
+
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private ConnectionDetector connectionDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-
-            TextView textView = findViewById(R.id.hello);
-
-
-           textView.setOnClickListener(new View.OnClickListener() {
-               @Override
-               public void onClick(View v) {
-                   showDialog();
-               }
-           });
+        connectionDetector =new ConnectionDetector(this);
+        if(connectionDetector.isConnectingToInternet()) {
+            findViewById(R.id.no_internet_cv).setVisibility(View.GONE);
+            findViewById(R.id.dashboard_cv).setVisibility(View.VISIBLE);
+        }else{
+            findViewById(R.id.dashboard_cv).setVisibility(View.GONE);
+            findViewById(R.id.no_internet_cv).setVisibility(View.VISIBLE);
         }
+
+
+
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.rcv_dashboard);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        String[] myDataset = {"Total Order : 805","Total Outlets : 20","Total Sale Amount : 500,000"};
+        mAdapter = new DashboardRcvAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.dashboard_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.date_icon:
+                showDialog();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
 
 
---
+++ app/src/main/java/com/muzammil/teamtnt/am/DashboardRcvAdapter.java
@@ -0,0 +1,55 @@
+package com.muzammil.teamtnt.am;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DashboardRcvAdapter extends RecyclerView.Adapter<DashboardRcvAdapter.DashboardRcvViewHolder> {
+    private String[] mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class DashboardRcvViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mTextView;
+        public DashboardRcvViewHolder(View v) {
+            super(v);
+            mTextView = v.findViewById(R.id.textView);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public DashboardRcvAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public DashboardRcvAdapter.DashboardRcvViewHolder onCreateViewHolder(ViewGroup parent,
+                                                     int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.dashboard_rcv_items, parent, false);
+
+        DashboardRcvViewHolder vh = new DashboardRcvViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(DashboardRcvViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.mTextView.setText(mDataset[position]);
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
